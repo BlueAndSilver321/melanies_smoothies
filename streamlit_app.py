b83c268411440dcfcdaa4b3c39e836df549860d1
BlueAndSilver321 <connorradecki@gmail.com>
2025-10-06 13:43:16 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,26 +34,25 @@ ingredients_list = st.multiselect(
     )
 
 if ingredients_list:
-    
-        ingredients_string = ''
+    ingredients_string = ''
 
-        for fruit_chosen in ingredients_list:
-                ingredients_string += fruit_chosen + ' '
-                smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/")
-                sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-                #st.write(ingredients_string)
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        #st.write(ingredients_string)
 
-        my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-                values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
-        st.write(my_insert_stmt)
-        #st.stop()
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
+    st.write(my_insert_stmt)
+    #st.stop()
 
-        #st.write(my_insert_stmt)
-        time_to_insert = st.button("Submit Order")
+    #st.write(my_insert_stmt)
+    time_to_insert = st.button("Submit Order")
         
-        if time_to_insert:
-            session.sql(my_insert_stmt).collect()
-            st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
